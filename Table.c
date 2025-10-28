a59882690a080fd86f9d71d4cc822ce023e0ac42
Sahil-242-ops <sahilbansal248@gmail.com>
2025-10-28 15:23:12 +0530

    Add files via upload

---
+++ Table.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+void mult(){
+    int n;
+    printf("Enter a number:");
+    scanf("%d",&n);
+    for(int i = 1;i<=10;i++){
+        printf("%d * %d = %d\n",n,i,n*i);
+    
+    }
+}
+int main(){
+    {mult(5);}
+return 0 ;
+}
